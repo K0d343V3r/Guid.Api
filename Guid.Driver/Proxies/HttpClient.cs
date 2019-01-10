fa5b84816496000620cd152fd7600db43c3d419b
K0d343V3r <zoolook@cox.net>
2019-01-10 15:20:47 -0800

    Initial commit.

---
+++ Guid.Driver/Proxies/HttpClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guid.Driver.Proxies
+{
+    public class HttpClient : System.Net.Http.HttpClient
+    {
+        public static readonly HttpClient Instance = new HttpClient();
+
+        static HttpClient()
+        {
+            Instance.BaseAddress = new Uri("https://localhost:44384");
+        }
+    }
+}
