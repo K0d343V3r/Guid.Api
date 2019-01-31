6748af4d3446b1c7cd61f3d839db9825fcd60b4f
K0d343V3r <zoolook@cox.net>
2019-01-31 13:11:27 -0800

    Added driver app to repo.

---
+++ Guid.Api.Driver/Proxies/HttpClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guid.Driver.Proxies
+{
+    /// <summary>
+    /// HttpClient singleton.
+    /// </summary>
+    public class HttpClient : System.Net.Http.HttpClient
+    {
+        public static readonly HttpClient Instance = new HttpClient();
+
+        static HttpClient()
+        {
+            Instance.BaseAddress = new Uri("https://localhost:44384");
+        }
+    }
+}
